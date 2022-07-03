e9d691aa331df2d75aa057a21f5cb2348c1836b8
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-07-03 11:45:13 +0700

    fix bug undefined

--- functions/api/services/alerts/whale.js
+++ functions/api/services/alerts/whale.js
@@ -7,7 +7,7 @@ const _ = require('lodash');
 // import config
 const config = require('config-yml');
 // import utils
-const { equals_ignore_case, name } = require('../../utils');
+const { number_format, equals_ignore_case, name } = require('../../utils');
 
 const donation_keywords = ['charity','donation','donate'];
 const hacked_keywords = ['hack'];
